0223606770ff0c74886f41e32f8f2959150fb1a1
william-pope <willpope@Wills-MacBook-Pro.local>
2019-04-17 15:28:32 -0700

    adding blinky.py

---
+++ blinky.py
@@ -0,0 +1,9 @@
+import RPi.GPIO as gpio
+import time
+gpio.setmode(GPIO.BCM)
+gpio.setwarnings(False)
+gpio.setup(18,gpio.OUT)
+gpio.output(18,gpio.HIGH)
+time.sleep(3)
+gpio.output(18,gpio.LOW)
+print "done"

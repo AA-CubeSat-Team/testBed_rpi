e9874c5e2cff6789009a4cfb5d912dfe21c901be
william-pope <willpope@Wills-MacBook-Pro.local>
2019-05-01 14:37:45 -0700

    add pwm1.py

--- pwm1.py
+++ pwm1.py
@@ -1,15 +1,15 @@
 import RPi.GPIO as IO        
-import time                            #calling time to provide delays in program
-IO.setwarnings(False)           #do not show any warnings
-IO.setmode (IO.BCM)         #we are programming the GPIO by BCM pin numbers. (PIN35 as ‘GPIO19’)
-IO.setup(18,IO.OUT)           # initialize GPIO19 as an output.
-p = IO.PWM(18,100)          #GPIO19 as PWM output, with 100Hz frequency
-p.start(0)                              #generate PWM signal with 0% duty cycle
-while 1:                               #execute loop forever
-    for x in range (50):                          #execute loop for 50 times, x being incremented from 0 to 49.
-        p.ChangeDutyCycle(x)               #change duty cycle for varying the brightness of LED.
-        time.sleep(0.1)                           #sleep for 100m second
+import time                            
+IO.setwarnings(False)                 
+IO.setmode (IO.BCM)                   
+IO.setup(18,IO.OUT)           
+p = IO.PWM(18,100)          
+p.start(0)                              
+while 1:                               
+    for x in range (50):                          
+        p.ChangeDutyCycle(x)              
+        time.sleep(0.1)                          
       
-    for x in range (50):                         #execute loop for 50 times, x being incremented from 0 to 49.
-        p.ChangeDutyCycle(50-x)        #change duty cycle for changing the brightness of LED.
-        time.sleep(0.1)                 #sleep for 100m second
+    for x in range (50):                        
+        p.ChangeDutyCycle(50-x)       
+        time.sleep(0.1)                 

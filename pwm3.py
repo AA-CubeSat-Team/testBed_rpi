3d2bb0bb44b4ff7f29cb65479953677566a2c990
william-pope <willpope@Wills-MacBook-Pro.local>
2019-05-15 15:02:56 -0700

    add Esc-new

--- pwm3.py
+++ pwm3.py
@@ -1,18 +1,10 @@
 import RPi.GPIO as GPIO     	# Importing RPi library to use the GPIO pins
 from time import sleep  	# Importing sleep from time library
-led_pin = 18            	# Initializing the GPIO pin 18 for LED
+pwm_pin = 18            	# Initializing the GPIO pin 18 for LED
 GPIO.setmode(GPIO.BCM)         	 # We are using the BCM pin numbering
 GPIO.setup(led_pin, GPIO.OUT)  	 # Declaring pin 21 as output pin
-pwm = GPIO.PWM(led_pin, 100)   	 # Created a PWM object
-pwm.start(50)                   	 # Started PWM at 0% duty cycle, "off"
-sleep(3) 
-LOW = 20
-MEDIUM = 50
-HIGH = 100
-dc = int(input("Change brightness [LOW MEDIUM HIGH]: "))
-pwm.ChangeDutyCycle(dc)
-sleep(5)
+pwm = GPIO.PWM(pwm_pin, 100)   	 # Created a PWM object
+pwm.start(0)                   	 # Started PWM at 0% duty cycle, "off"
 while 1:
-	dc = int(input("Change brightness [LOW MEDIUM HIGH]: "))
+	dc = int(input("Change duty cycle: "))
 	pwm.ChangeDutyCycle(dc)
-	sleep(5)

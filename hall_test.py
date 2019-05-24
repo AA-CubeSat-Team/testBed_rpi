975cd8566049259cc5173921e653a9c48e25394a
william-pope <willpope@Wills-MacBook-Pro.local>
2019-05-24 15:19:37 -0700

    add calibrate function script

---
+++ hall_test.py
@@ -0,0 +1,15 @@
+#hall_test.py
+
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+
+def button_callback(channel):
+    print("Button was pushed!")
+
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set GPIO6 to be an input pin and set initial value to be pulled low (off)
+
+GPIO.add_event_detect(6,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+
+message = input("Press enter to quit\n\n") # Run until someone presses enter
+
+GPIO.cleanup() # Clean up

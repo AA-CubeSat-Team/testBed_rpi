144886f3f000c0d98ac6aee9b3dadc4b4ccdf910
william-pope <willpope@Wills-MacBook-Pro.local>
2019-05-24 16:39:03 -0700

    add calibrate function script

--- hall_test.py
+++ hall_test.py
@@ -5,9 +5,10 @@ import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set GPIO6 to be an input pin and set initial value to be pulled low (off)
+GPIO.add_event_detect(6, GPIO.RISING)
 
 while True:
-    if GPIO.event_detected(channel):
+    if GPIO.event_detected(6):
         num += 1
         print(num)
 

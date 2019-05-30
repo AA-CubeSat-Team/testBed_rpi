cc251406a2be87c71305c26dae99f65cbbece197
David Albers <Davidealbers@gmail.com>
2019-05-30 15:23:51 -0700

    switced from polling event status to having an event handler

--- hall_test.py
+++ hall_test.py
@@ -1,16 +1,18 @@
 #hall_test.py
 
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import time
+def EventHandler(pin):
+    num += 1
+    print(num)
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set GPIO6 to be an input pin and set initial value to be pulled low (off)
 GPIO.add_event_detect(6, GPIO.RISING)
-
+GPIO.add_event_callback(6,EventHandler,10)
 num = 0
 while True:
-    if GPIO.event_detected(6):
-        num += 1
-        print(num)
-
+    print("I'm alive!")#This
+    time.sleep(2)
 GPIO.cleanup() # Clean up

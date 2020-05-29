094f0125dd5b57d44f54c173cc777cb29c085f5b
Will Pope <wpope@uw.edu>
2020-05-29 11:53:11 -0700

    update to spiTest.py

--- spiTest.py
+++ spiTest.py
@@ -6,4 +6,15 @@ import spidev
 bus = 0
 device = 0      # slave select pin
 
-spi = spidev.SpiDev()
+spi = spidev.SpiDev()       # enables spi, creates "spi" object
+
+spi.open(bus, device)       # opens connection on specified bus, device
+
+spi.max_speed_hz = 250000   # sets master freq at 250 kHz, must be (150:300) kHz
+spi.mode = 0
+
+msg = [0xff]
+spi.xfer2(msg)
+
+print('transfer complete')
+

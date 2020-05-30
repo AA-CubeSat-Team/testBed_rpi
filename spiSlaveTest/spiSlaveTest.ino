4b6d73c8cb319490fdfdff0e4269fd18557de3ba
Will Pope <wpope@uw.edu>
2020-05-29 18:23:57 -0700

    updating spiTest.py

--- spiSlaveTest/spiSlaveTest.ino
+++ spiSlaveTest/spiSlaveTest.ino
@@ -1,8 +1,8 @@
 # include <SPI.h>
 
 volatile boolean received;
-volatile byte Slavereceived;
-volatile byte Slavesend;
+volatile byte rcv;
+volatile byte rpl;
 
 void setup() {
   Serial.begin(115200);   // standard serial monitor set up
@@ -13,38 +13,36 @@ void setup() {
   received = false;
 
   SPI.attachInterrupt();  // if data received from master, interrupt is triggered
+  Serial.println("Setup complete");
 }
 
 
 ISR (SPI_STC_vect) {      // interrupt service routine
-  Slavereceived = SPDR;   // byte from transfer                
-  received = true;                       
+  rcv = SPDR;   // byte from transfer                
+  Serial.println(rcv);
+  
+  rpl = 2;
+  SPDR = rpl;             // transfers rpl back up MISO
+  
+  received = true; 
+  Serial.println("received");                      
 }
 
+// ISSUE: not triggering interrupt
+// CHECK: wiring/pins, interrupt configured correctly, RPi transferrring correctly,
+//        compatibility of signals, 5V vs 3.3V
 
 void loop() {
+/*  
   if(received) {                           // logic to SET LED ON OR OFF depending upon the value recerived from master
-    if (Slavereceived == 1) {              // "1" is the contents of the SPI transfer
+    if (rcv == 1) {              // "1" is the contents of the SPI transfer
         Serial.println("Slave LED ON");
     }
     else {
         Serial.println("Slave LED OFF");
-    }
-/*
-    buttonvalue = digitalRead(buttonpin);  // reads the status of the pin 2
-      
-      if (buttonvalue == HIGH) {           // logic to set the value of x to send to master
-        x = 1;
-      }  
-      else {
-        x = 0;
-      }
-      
-    Slavesend = x;                             
-    SPDR = Slavesend;                      // sends the x value to master via SPDR 
-*/    
+    }    
     delay(1000);
   }
-
-  
+  Serial.println(received);
+*/  
 }
--- spiTest.py
+++ spiTest.py
@@ -10,7 +10,7 @@ spi = spidev.SpiDev()       # enables spi, creates "spi" object
 
 spi.open(bus, device)       # opens connection on specified bus, device
 
-spi.max_speed_hz = 250000   # sets master freq at 250 kHz, must be (150:300) kHz
+spi.max_speed_hz = 250000   # sets master freq at 250 kHz, must be (150:300) kHz for RWA
 spi.mode = 0
 
 while True:

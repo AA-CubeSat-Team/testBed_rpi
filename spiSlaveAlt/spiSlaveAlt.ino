c144960e6bc7c8817ec472e9c4dfac0ce2f55bee
Will Pope <wpope@uw.edu>
2020-07-04 16:21:54 -0700

    updating spiTest.py

--- spiSlaveAlt/spiSlaveAlt.ino
+++ spiSlaveAlt/spiSlaveAlt.ino
@@ -1,20 +1,21 @@
 #include<SPI.h>  
 
-byte reqArr[9];
+volatile byte reqArr[9];
+volatile byte rplArr[4] = {9,8,7,6};
 volatile byte reqB;
 volatile byte rplB;
 volatile bool flag;
 volatile byte kk;
 
-// CURRENT STATE: returns manipilated request in correct form
-// NEXT STEP: needs to be robust to changes in request length, eventually build in constant 0x00s
+// CURRENT STATE: returns manipulated request in correct form
+// NEXT STEP: needs to be robust to changes in request length, eventually build in constant 0x7Es
 void setup (void){
   Serial.begin (115200);
   
   pinMode(MISO, OUTPUT);
 
   SPCR |= _BV(SPE);  
-  SPCR |= _BV(SPIE);
+  SPI.attachInterrupt();
 
   SPDR = 0;
   flag = false;
@@ -29,24 +30,29 @@ ISR (SPI_STC_vect){
   reqArr[kk] = reqB;
   kk++;
   
-  if (kk == 9){
+  if (reqB == 126){
+    SPI.detachInterrupt();
     flag = true;
+    Serial.println("flag");
   }
-
-  rplB = reqB * 10;
+  
+  rplB = reqB+2;                     // finishes ISR even after interrupt is detached
   SPDR = rplB;
 }
-
+                                      // cannot execute void loop() until done receiving bytes
 
 void loop (void){
   if (flag == true){
     Serial.print("req: ");
-    for (int jj = 0; jj < 9; jj++){
+    for (int jj = 0; jj < sizeof(reqArr); jj++){
       Serial.print(reqArr[jj]);
       Serial.print(" ");
+      reqArr[jj] = 0;
     }
     Serial.println();
+    SPDR = 0;
     flag = false;
     kk = 0;
+    SPI.attachInterrupt();
   }
 }

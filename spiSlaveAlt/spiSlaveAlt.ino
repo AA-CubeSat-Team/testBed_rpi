fd06a6c6b645732757856ea932b4a16db7094f90
Will Pope <wpope@uw.edu>
2020-07-05 21:11:13 -0700

    updating spiTest.py

--- spiSlaveAlt/spiSlaveAlt.ino
+++ spiSlaveAlt/spiSlaveAlt.ino
@@ -4,56 +4,45 @@ volatile byte reqArr[9];
 volatile byte rplArr[4] = {9,8,7,6};
 volatile byte reqB;
 volatile byte rplB;
-volatile bool flag;
+volatile byte flag;
 volatile byte kk;
+volatile byte readReg;
 
-// CURRENT STATE: returns manipulated request in correct form
-// NEXT STEP: needs to be robust to changes in request length, eventually build in constant 0x7Es
+// CURRENT STATE: recognizes flag, cuts interrupts to process data
+// NEXT STEP: test on long 0x7E requests, input own reply package
+
 void setup (void){
   Serial.begin (115200);
   
   pinMode(MISO, OUTPUT);
 
-  Serial.print("SPE: ");           // SPCR is 1 byte, 8 bits, where each bit controls something
-  Serial.println(SPE, BIN);
   SPCR = SPCR | bit(SPE);           // sets SPE bit in SPCR to 1, enabling SPI
   SPCR = SPCR | bit(SPIE);          // sets SPIE bit in SPCR to 1, enabling SPI interrupts
-  Serial.print("SPCR: ");
-  Serial.println(SPCR, BIN);
   
   SPDR = 0;
   flag = false;
   kk = 0;
 
   Serial.println("Setup complete");
-  Serial.println(bit(SPIE));
 }  
 
 ISR (SPI_STC_vect){
   reqB = SPDR;
-  
   reqArr[kk] = reqB;
   kk++;
   
   if (reqB == 126){
-    SPI.detachInterrupt();
-    flag = true;
-    Serial.println("flag");
-  }
-
-  if (reqB == 8){
-    SPI.detachInterrupt();
-    flag = true;
-    Serial.println("flag");
+    SPI.detachInterrupt();          // sets SPIE bit in SPCR to 0, disabling interrupts 
+    flag = 1;
   }
   
-  rplB = reqB+2;                     // finishes ISR even after interrupt is detached
+  rplB = reqB+2;                    
   SPDR = rplB;
 }
-                                      // cannot execute void loop() until done receiving bytes
+                                     
 
 void loop (void){
-  if (flag == true){
+  if (flag == 1){
     Serial.print("req: ");
     for (int jj = 0; jj < sizeof(reqArr); jj++){
       Serial.print(reqArr[jj]);
@@ -61,11 +50,13 @@ void loop (void){
       reqArr[jj] = 0;
     }
     Serial.println();
+    
     SPDR = 0;
     flag = false;
     kk = 0;
-    delay(500);                      // interrupt flag is set even when interrupts are detached
-    //SPIF |= 1;
-    SPI.attachInterrupt();            // retriggers interrupt before transmission is over
+    
+    readReg = SPSR;
+    readReg = SPDR;
+    SPCR = SPCR | bit(SPIE);            
   }
 }

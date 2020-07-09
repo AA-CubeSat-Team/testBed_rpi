c2b4a035e2f976072c46b00b5518840f2355a267
Will Pope <wpope@uw.edu>
2020-07-08 22:57:33 -0700

    updating spiTest.py

--- spiSlaveAlt/spiSlaveAlt.ino
+++ spiSlaveAlt/spiSlaveAlt.ino
@@ -2,7 +2,8 @@
 
 volatile byte reqArr[9];        // robust to changes in req length, reqArr fills w 0s after flag
 volatile byte rplArr[4] = {9,8,7,6};
-volatile byte reqB;
+volatile byte reqB_new;
+volatile byte reqB_old;
 volatile byte rplB;
 volatile byte flag;
 volatile byte kk;
@@ -20,12 +21,62 @@ void setup (void){
   SPCR = SPCR | bit(SPIE);          // sets SPIE bit in SPCR to 1, enabling SPI interrupts
   
   SPDR = 0;
-  flag = false;
+  flag = 0;
   kk = 0;
+  reqB_old = 126;
 
-  Serial.println("Setup complete");
+  Serial.println(reqB_old);
 }  
 
+
+
+ISR (SPI_STC_vect){
+  reqB_new = SPDR;
+
+  if ( (reqB_old == 126) && (reqB_new == 126) ){       
+    // set reply mode
+  }
+  if ( (reqB_old == 126) && (reqB_new != 126) ){  
+    reqArr[kk] = reqB_new;
+    kk++;
+  }
+  if ( (reqB_old != 126) && (reqB_new != 126) ){  
+    reqArr[kk] = reqB_new;
+    kk++;
+  }
+  if ( (reqB_old != 126) && (reqB_new == 126) ){  
+    SPI.detachInterrupt();          
+    flag = 1;
+  }
+  
+  reqB_old = reqB_new;
+
+  SPDR = 126;
+}
+                                     
+//SPI.detachInterrupt();
+void loop (void){
+  if (flag == 1){
+    Serial.print("req: ");
+    for (int jj = 0; jj < sizeof(reqArr); jj++){
+      Serial.print(reqArr[jj]);
+      Serial.print(" ");
+      reqArr[jj] = 0;
+    }
+    Serial.println();
+    
+    SPDR = 0;
+    flag = false;
+    kk = 0;
+    reqB_old = 126;
+    
+    readReg = SPSR;
+    readReg = SPDR;
+    SPCR = SPCR | bit(SPIE);            
+  }
+}
+
+/*
 ISR (SPI_STC_vect){
   reqB = SPDR;
   reqArr[kk] = reqB;
@@ -60,3 +111,4 @@ void loop (void){
     SPCR = SPCR | bit(SPIE);            
   }
 }
+*/

47c29f9549716937dfdc6964c63dfaaa126d90b0
Will Pope <wpope@uw.edu>
2020-07-22 22:01:30 -0700

    updating RWA scripts

--- dumbTest.py
+++ dumbTest.py
@@ -1,11 +1,15 @@
 
 # LIST FLATTENING TOOL
 import collections
-def flatList(inpArr1):
+def flatList1(inpArr1):
     if isinstance(inpArr1, collections.Iterable):
         return [a for i in inpArr1 for a in flatList(i)]
     else:
         return [inpArr1]
+
+def flatList2(inpArr1):
+    newlist = [j for i in inpArr1 for j in i]
+    return newlist
 
 
 print(flatList([1, ['abc', 4], 5]))

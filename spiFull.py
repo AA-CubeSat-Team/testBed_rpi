1053b6d735adfb478dc2804a813066ef017983d7
Will Pope <wpope@uw.edu>
2020-07-26 16:05:08 -0700

    updating RWA scripts

--- spiFull.py
+++ spiFull.py
@@ -666,19 +666,19 @@ while True:
                 runSensors = 1
 
                 for speedInp in range(10000, 70000, 5000):
-                    if nominalState = False:
+                    if nominalState == False:
                         print("nominalState: ", nominalState)
 
-                    if nominalState = True:
+                    if nominalState == True:
                         processAuto(6, speedInp, 0)
                         print("speedInp: ", speedInp)
                         time.sleep(1)
 
                 for speedInp in range(65000, 5000, -5000):
-                    if nominalState = False:
+                    if nominalState == False:
                         print("nominalState: ", nominalState)
 
-                    if nominalState = True:
+                    if nominalState == True:
                         processAuto(6, speedInp, 0)
                         print("speedInp: ", speedInp)
                         time.sleep(1)

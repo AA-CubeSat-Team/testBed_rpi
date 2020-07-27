ba1c5b9ca52116644ccdba804711bbabed0f5519
Will Pope <wpope@uw.edu>
2020-07-26 22:27:25 -0700

    updating RWA scripts

--- spiFull.py
+++ spiFull.py
@@ -197,7 +197,7 @@ def spiTransfer(reqArr1,rplN1):
     time.sleep(0.100)                           # waits 100 ms for RWA to process
        
     rplArrX = spi.xfer2(msrEmpArr)
-    print(rplArrX)
+    print("rplArrX: ",rplArrX)
     rplArrH = xorSwitch(rplArrX, "rplMode")   
     rplArr1 = rplArrH[(0+2):(rplN1+2)] 
 

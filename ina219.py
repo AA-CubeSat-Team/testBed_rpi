b7c280710eb333c49587262bd7fe23352ea29770
Will Pope <wpope@uw.edu>
2020-07-27 22:02:04 -0700

    updating RWA scripts

--- ina219.py
+++ ina219.py
@@ -8,11 +8,11 @@ SHUNT_OHMS = 0.1
 def read():
    ina = INA219(SHUNT_OHMS)
    ina.configure()
-   print "Bus Voltage: %.3f V" % ina.voltage()
+   print("Bus Voltage: %.3f V", ina.voltage())
    try:
-       print "Bus Current: %.3f mA" % ina.current()
-       print "Power: %.3f mW" % ina.power()
-       print "Shunt voltage: %.3f mV" % ina.shunt_voltage()
+       print("Bus Current: %.3f mA", ina.current())
+       prin("Power: %.3f mW", ina.power())
+       prin("Shunt voltage: %.3f mV", ina.shunt_voltage())
    except DeviceRangeError as e:
        # Current out of device range with specified shunt resister
        print e

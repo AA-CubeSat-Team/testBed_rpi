ce91704e5293ee9db802959ea4138887539636bd
Will Pope <wpope@uw.edu>
2020-07-29 21:57:44 -0700

    updating RWA scripts

--- ina219.py
+++ ina219.py
@@ -6,8 +6,9 @@ import adafruit_ina219
 i2c = busio.I2C(board.SCL, board.SDA) 
 ina219 = adafruit_ina219.INA219(i2c)
 
+# ina219.bus_voltage
 print("Bus Voltage: {} V".format(ina219.bus_voltage)) 
 #print("Shunt Voltage: {} mV".format(ina219.shunt_voltage / 1000)) 
 print("Current: {} mA".format(ina219.current))
 
-print(ina219.bus_voltage)
+print(type(ina219.bus_voltage))
